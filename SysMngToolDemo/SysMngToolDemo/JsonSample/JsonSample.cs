006e04bb4702b6ce0c3c4d50a28c765d79f6191c
木原　仁 <ts-hitoshi.kihara@rakuten.com>
2019-06-12 18:35:34 +0900

    20190612作業コミット

---
+++ SysMngToolDemo/SysMngToolDemo/JsonSample/JsonSample.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Json;
+using System.IO;
+
+namespace SysMngToolDemo.JsonSample
+{
+    // [C#] C#でJSONを扱う方法まとめ
+    // https://dev.classmethod.jp/etc/c-sharp-json/
+    public class JsonSampleClass
+    {
+        public JsonSampleClass()
+        {
+            var p = new Person();
+            p.Name = "Kihara Hitoshi";
+            p.Age = 50;
+
+            using(var ms = new MemoryStream())
+            using(var sr = new StreamReader(ms))
+            {
+                var serializer = new DataContractJsonSerializer(typeof(Person));
+                serializer.WriteObject(ms, p);
+                ms.Position = 0;
+
+                var json = sr.ReadToEnd();
+
+                System.Console.WriteLine(json.ToString());
+
+                ms.Position = 0;
+                var deserialized = (Person)serializer.ReadObject(ms);
+
+                System.Console.WriteLine("Name : {0} / Age : {1}", deserialized.Name, deserialized.Age);
+                
+            }
+        }
+    }
+
+}
---
+++ SysMngToolDemo/SysMngToolDemo/JsonSample/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace SysMngToolDemo.JsonSample
+{
+    // [C#] C#でJSONを扱う方法まとめ
+    // https://dev.classmethod.jp/etc/c-sharp-json/
+    [DataContract]
+    public class Person
+    {
+        [DataMember(Name="name")]
+        public string Name { get; set; }
+
+        [DataMember(Name = "age")]
+        public int Age { get; set; }
+    }
+}
